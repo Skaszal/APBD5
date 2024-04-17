ceb3419a6122f9a9f6758e09c62fd5309d6bedb0
s27168 <s27168@pjwstk.edu.pl>
2024-04-17 22:59:39 +0200

    Deleted unused Classes

--- Models/Visit.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace APBD5.Models;
-
-public class Visit
-{
-    public DateTime DateOfVisit {get; set;}
-    public Animal Animal { get; set; }
-    public string Description { get; set; }
-    public float cost { get; set; }
-}
